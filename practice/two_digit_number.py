11044b6d176b92a6ebfa498e2069d45f8a576e2e
Mehedi Hasan Faysal <mhfemu@gmail.com>
2021-04-13 21:21:23 +0600

    Create two_digit_number.py

---
+++ practice/two_digit_number.py
@@ -0,0 +1,5 @@
+#Write a program that adds the digits in a 2 digit number.
+#########################################################
+
+two_digit_number = input("Type a two digit number: ")
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
