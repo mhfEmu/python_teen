ccf68fda7d73592406e983265a2b7379be6fcd55
iMahdi <hasanmehedi699@gmail.com>
2021-04-16 21:27:39 +0600

    Complete Rollar Coaster in Console

--- practice/rollarcoaster_ticket_system.py
+++ practice/rollarcoaster_ticket_system.py
@@ -1,29 +1,28 @@
 # Complete Rollar Coaster in Console
-Height = int(input("What's your Height in CM? "))
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
 bill = 0
 
-if Height >= 120:
-    print("You can ride the RollarCoaster!")
-    age = int(input("What's your age in Years? "))
-    if age <= 12:
-        bill = 6
-
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        bill = 10
-
-    elif age < 40:
+        bill = 7
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
+    else:
         bill = 12
-    else:
-        bill = 13
+        print("Adult tickets are $12.")
 
-    if age > 45 and age < 55:
-        print(f"You're getting a free ride!")
-    else:
-        photo = input("You want photo while riding? Y/n \n")
-        if photo == "Y":
-            print(f"Total ticket price is ${bill+3}.")
-        else:
-            print(f"Your ticket price is ${bill}.")
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
 
 else:
-    print("Can't ride")
+    print("Sorry, you have to grow taller before you can ride.")
