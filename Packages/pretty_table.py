b5558d959a29d528451d2c02dd04fa6a86bd9ffe
iMahdi <hasanmehedi699@gmail.com>
2021-04-19 23:16:07 +0600

    This is Python Package PrettyTable

--- Packages/pretty_table.py
+++ Packages/pretty_table.py
@@ -3,5 +3,8 @@ from prettytable import PrettyTable
 my_table = PrettyTable()
 my_table.add_column("Pokemon Master", ["Pikachu", "Sharpedo", "Charizard"])
 my_table.add_column("Attributes", ["Electric", "Water", "Fire"])
+# print(my_table.align)
+
+my_table.align = "l"
 
 print(my_table)
