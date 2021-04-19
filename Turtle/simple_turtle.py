1d1f045506a28ad817af878acffd1ee522e3d237
iMahdi <hasanmehedi699@gmail.com>
2021-04-19 21:34:55 +0600

    This is Python Turtle Practice

---
+++ Turtle/simple_turtle.py
@@ -0,0 +1,11 @@
+# Seeing python turtle in action
+from turtle import Turtle, Screen
+
+mustofa = Turtle()
+print(mustofa)
+mustofa.shape("turtle")
+mustofa.color("red")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
