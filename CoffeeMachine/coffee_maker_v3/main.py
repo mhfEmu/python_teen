96dc4fbea37ec475a7a314b9483d727cb636431a
iMahdi <hasanmehedi699@gmail.com>
2021-04-19 19:28:53 +0600

    This is Version 3 which is with OOP

---
+++ CoffeeMachine/coffee_maker_v3/main.py
@@ -0,0 +1,10 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# TODO: 1 print report
+# TODO: 2 Check resource sufficient?
+# TODO: 3 Process Coins
+# TODO: 4 Check transaction successful?
+# TODO: 5 Make Coffee
+print("love")
