9406f3d53b3e62ec219e3ec28de45cc7d901ce74
iMahdi <hasanmehedi699@gmail.com>
2021-04-21 03:40:50 +0600

    This is Version 1 of a Quiz System

--- Quiz/Quiz V1/quiz_brain.py
+++ Quiz/Quiz V1/quiz_brain.py
@@ -4,7 +4,15 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
 
+    def question_left(self):
+        return len(self.question_list) > self.question_number
+        # if len(self.question_list) == self.question_number:
+        #     return False
+        # return True
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        self.question_number += 1
-        input(f"Q,{self.question_number}: {current_question.text} (True/ False): ")
+
+        while self.question_left():
+            self.question_number += 1
+            input(f"Q,{self.question_number}: {current_question.text} (True/ False): ")
