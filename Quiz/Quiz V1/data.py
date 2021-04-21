add6ec2bb37e191598fe0bbfc07d67a4ce88215f
iMahdi <hasanmehedi699@gmail.com>
2021-04-22 02:38:36 +0600

    This is Version 1 of a Quiz System

--- Quiz/Quiz V1/data.py
+++ Quiz/Quiz V1/data.py
@@ -1,16 +1,54 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
-             "you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, "
-             "you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {"category": "Science: Computers",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "The first computer bug was formed by faulty wires.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book "
+                 "form. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "RAM stands for Random Access Memory.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "AMD created the first consumer 64-bit processor.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "Android versions are named in alphabetical order.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, "
+                 "and Chrome OS.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+     "correct_answer": "True", "incorrect_answers": ["False"]}
 ]
+
+#     [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+#              "you are free to take it home to eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, "
+#              "you are entitled to a state funeral.", "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
--- Quiz/Quiz V1/main.py
+++ Quiz/Quiz V1/main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     # from class let's create ne object
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
@@ -16,4 +16,4 @@ while quiz.question_left():
     quiz.next_question()
 
 print("You've successfully completed the Quiz")
-print(f"Your final score is {quiz.score} out of {quiz.question_number}")
+print(f"Your final score is {quiz.score}/ {quiz.question_number}")
