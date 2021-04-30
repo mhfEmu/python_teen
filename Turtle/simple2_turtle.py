8c216d52deb85e6d69ca6761319b46f4dee20b49
iMahdi <hasanmehedi699@gmail.com>
2021-04-30 21:16:10 +0600

    Drawing a square

--- Turtle/simple2_turtle.py
+++ Turtle/simple2_turtle.py
@@ -1,3 +1,17 @@
-from turtle import Turtle
+# Drawing a square
+from turtle import Turtle, Screen
 
 mustofa_the_turtle = Turtle()
+mustofa_the_turtle.shape("turtle")
+mustofa_the_turtle.color("ForestGreen")
+mustofa_the_turtle.forward(100)
+mustofa_the_turtle.right(90)
+mustofa_the_turtle.forward(100)
+mustofa_the_turtle.right(90)
+mustofa_the_turtle.forward(100)
+mustofa_the_turtle.right(90)
+mustofa_the_turtle.forward(100)
+mustofa_the_turtle.right(90)
+
+screen = Screen()
+screen.exitonclick()
